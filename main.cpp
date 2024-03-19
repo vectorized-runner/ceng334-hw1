6a39f22327ebf85af6af874868590907b041fe27
Bartu Baş <bartubas99@gmail.com>
2024-03-19 16:28:13 +0300

    Simple bare-bones program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
+#include "parser.h"
+
+using namespace std;
 
 int main() {
-    // Your code here
-    std::cout << "Hello, World!" << std::endl;
-    return 0; // Indicates successful execution
+
+    string inputLine;
+    getline(cin, inputLine);
+
+    while(inputLine != "quit"){
+        /*
+        parsed_input* ptr = (parsed_input*)malloc(sizeof(parsed_input)); 
+        auto cPtr = const_cast<char*>(inputLine.c_str());
+        auto parse_success = parse_line(cPtr, ptr);
+
+        if(!parse_success){
+            cout << "Parse error for line: " << inputLine << endl;
+        }
+        */
+
+       cout << "Received Line: " << inputLine << endl;
+    }
+
+    return 0;
 }
