cdf5eb9629059e16edcbadec5ca47297bea0bc00
Bartu Baş <bartubas99@gmail.com>
2024-03-19 16:35:15 +0300

    Parsing line, pretty simple

--- main.cpp
+++ main.cpp
@@ -4,24 +4,37 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     string inputLine;
+
+    cout << "/> ";
     getline(cin, inputLine);
 
-    while(inputLine != "quit"){
-        /*
-        parsed_input* ptr = (parsed_input*)malloc(sizeof(parsed_input)); 
+    while (inputLine != "quit")
+    {
+        parsed_input* ptr = (parsed_input*)malloc(sizeof(parsed_input));
         auto cPtr = const_cast<char*>(inputLine.c_str());
         auto parse_success = parse_line(cPtr, ptr);
 
         if(!parse_success){
             cout << "Parse error for line: " << inputLine << endl;
         }
-        */
+        
+        pretty_print(ptr);
 
-       cout << "Received Line: " << inputLine << endl;
-       getline(cin, inputLine);
+        auto inputCount = ptr->num_inputs;
+        auto separator = ptr->separator;
+
+        cout << "InputCount: " << inputCount << endl;
+
+
+        // TODO: Remove
+        cout << "Received Line: " << inputLine << endl;
+
+        cout << "/> ";
+        getline(cin, inputLine);
     }
 
     return 0;
