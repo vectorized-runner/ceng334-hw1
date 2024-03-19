27b10a2a6ef932098241ba9d3ab1d72c2761a2c8
Bartu Baş <bartubas99@gmail.com>
2024-03-19 17:31:58 +0300

    WIP

--- main.cpp
+++ main.cpp
@@ -1,12 +1,61 @@
 #include <iostream>
 #include <string>
 #include "parser.h"
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 
+struct Command
+{
+};
+
+void runPipeline(const parsed_input *input)
+{
+    auto inputCount = (int)input->num_inputs;
+
+    for (int i = 0; i < inputCount; i++)
+    {
+        auto currentCommand = input->inputs[i];
+        auto type = currentCommand.type;
+
+        switch (type)
+        {
+            case INPUT_TYPE_SUBSHELL:
+                break;
+            case INPUT_TYPE_COMMAND:
+                break;
+            case INPUT_TYPE_PIPELINE:
+                break;
+            default:
+            {
+                cout << "COMMAND TYPE ERROR" << endl;
+                exit(-1);
+            }
+        }
+
+        auto pid = fork();
+        if (pid < 0)
+        {
+            cout << "FORK ERROR" << endl;
+            exit(-1);
+        }
+
+        pipe();
+
+        if (pid == 0)
+        {
+            // Child
+        }
+        else
+        {
+            // Parent
+        }
+    }
+}
+
 int main()
 {
-
     string inputLine;
 
     cout << "/> ";
@@ -14,28 +63,44 @@ int main()
 
     while (inputLine != "quit")
     {
-        parsed_input* ptr = (parsed_input*)malloc(sizeof(parsed_input));
-        auto cPtr = const_cast<char*>(inputLine.c_str());
+        parsed_input *ptr = (parsed_input *)malloc(sizeof(parsed_input));
+        auto cPtr = const_cast<char *>(inputLine.c_str());
         auto parse_success = parse_line(cPtr, ptr);
 
-        if(!parse_success){
+        if (!parse_success)
+        {
             cout << "PARSE ERROR. LINE: " << inputLine << endl;
-            exit(1);
+            exit(-1);
         }
 
         pretty_print(ptr);
 
         auto inputCount = ptr->num_inputs;
-        if(inputCount <= 0){
+        if (inputCount <= 0)
+        {
             cout << "UNEXPECTED INPUT COUNT: " << inputCount << endl;
-            exit(1);
+            exit(-1);
         }
 
-
         auto separator = ptr->separator;
-        
 
-
+        // TODO:
+        switch (separator)
+        {
+        case SEPARATOR_PARA:
+            break;
+        case SEPARATOR_PIPE:
+        {
+            runPipeline(ptr);
+            break;
+        }
+        case SEPARATOR_SEQ:
+            break;
+        default:
+            {
+                exit(-1);
+            }
+        }
 
         cout << "InputCount: " << inputCount << endl;
 
